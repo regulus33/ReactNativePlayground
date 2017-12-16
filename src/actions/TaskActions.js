c89340c97c04cd246a82f0e9313333e0dca9d0bb
Regulus33 <zacharyrowden89@gmail.com>
2017-12-16 13:39:23 -0500

    Fix bug in actions promise.

--- src/actions/TaskActions.js
+++ src/actions/TaskActions.js
@@ -1,6 +1,8 @@
-import firebase from 'firebase'
+import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
 import {
-  TASK_UPDATE
+  TASK_UPDATE,
+  TASK_CREATE
 } from './types';
 
 export const taskUpdate = ({ prop, value }) => {
@@ -15,7 +17,12 @@ export const taskCreate = ({name, insights, dueDate}) => {
   //the ref is not a url, its a path to your dataStore
   //more like and SQL query
   const { currentUser } = firebase.auth();
-
-  firebase.database().ref(`/users/${currentUser.uid}/tasks`)
-    .push({ name, insights, dueDate });
+  return (dispatch) => {
+    firebase.database().ref(`/users/${currentUser.uid}/tasks`).push({ name, insights, dueDate })
+      .then(() => {
+      // .then(() => Actions.taskList({ type: 'reset' }));
+        dispatch({ type: TASK_CREATE });
+        Actions.taskList();
+      });
+  };
 };
