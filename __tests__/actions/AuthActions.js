591f993b7fea85e4866e283a26d2a4ed45cd0f72
Regulus33 <zacharyrowden89@gmail.com>
2017-12-18 13:00:08 -0500

    Add first test. YAYAYAYAYA.

---
+++ __tests__/actions/AuthActions.js
@@ -0,0 +1,12 @@
+import 'react-native';
+import React from 'react';
+import { EMAIL_CHANGED } from '../../src/actions/types';
+import { emailChanged } from '../../src/actions';
+
+describe('auth actions', () => {
+    describe('emailChanged', () => {
+      it('has the correct type', () => {
+        expect(emailChanged().type).toEqual(EMAIL_CHANGED);
+      })
+    })
+})
